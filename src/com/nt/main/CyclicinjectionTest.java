593743b58fe55496453f8803bae77ac5da3f7616
abc592 <srikanth.chs123456789@gmail.com>
2025-08-16 21:52:37 +0530

    constructorInjection

--- src/com/nt/main/CyclicinjectionTest.java
+++ src/com/nt/main/CyclicinjectionTest.java
@@ -2,18 +2,20 @@ package com.nt.main;
 
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-import com.nt.sbeans.WishMessageGenerator;
+import com.nt.sbeans.A;
+import com.nt.sbeans.B;
 
-public class DependencyInjectionTest {
+public class CyclicinjectionTest {
 
 	public static void main(String[] args) {
 		// create IOC container
 		
 	FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext("src/com/nt/cfgs/applicationContext.xml");
-	Object obj = ctx.getBean("wmg");
-	WishMessageGenerator generator = (WishMessageGenerator)obj;
-	String result = generator.showWishMessage("Srikanth");
-			System.out.println(result);
+	A a = (A)ctx.getBean("a1");
+	System.out.println("a="+a);
+	System.out.println("==========");
+	B b = (B)ctx.getBean("b1");
+	System.out.println("b="+b);
 
 	}
 
---
+++ src/com/nt/sbeans/A.java
@@ -0,0 +1,20 @@
+package com.nt.sbeans;
+
+public class A {
+	private B b;
+	
+	public A() {
+		System.out.println("A::0-Param constructor");
+	}
+	
+	/*public void setB(B b) {
+		System.out.println("A.setB()");
+		this.b=b;
+		
+	}
+	*/
+	public String toString() {
+		return "From A";
+	}
+
+}
