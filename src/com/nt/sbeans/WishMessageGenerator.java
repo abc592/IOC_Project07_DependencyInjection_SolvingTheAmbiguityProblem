f43c43e707bec0a1af1e3157b507922d62573e01
abc592 <srikanth.chs123456789@gmail.com>
2025-08-16 20:08:43 +0530

    added constructor logic

--- src/com/nt/sbeans/WishMessageGenerator.java
+++ src/com/nt/sbeans/WishMessageGenerator.java
@@ -1,18 +1,32 @@
 package com.nt.sbeans;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class WishMessageGenerator {
-	//HAS-A property
-	private LocalTime time;
-	
+    private LocalTime time;
+    private LocalDate date;
+    private int age;
+    private String email;
+    
+    //ConstructorInjection
+    public WishMessageGenerator(LocalTime time, LocalDate date, int age, String email) {
+    	System.out.println("WishMessageGenerator.WishMessageGenerator()");
+        this.time = time;
+        this.date = date;
+        this.age = age;
+        this.email = email;
+    }
+
 	//setter method for setter Injection
-	public void setTime(LocalTime time) {
-		this.time=time;
-	}
+//	public void setTime(LocalTime time) {
+//		this.time=time;
+//	}
 	// develop the b.method
+	
 	public String showWishMessage(String user) {
 		//get current hour of the day
+		System.out.println("WishMessageGenerator.showWishMessage1()::"+time+"...."+date+"...."+age+"...."+email);
 		int hour = time.getHour();
 		if(hour<12)
 			return "Good Morning::"+user;
@@ -23,4 +37,6 @@ public class WishMessageGenerator {
 		else
 			return "Good Night::"+user;
 	}
+	
+	
 }
