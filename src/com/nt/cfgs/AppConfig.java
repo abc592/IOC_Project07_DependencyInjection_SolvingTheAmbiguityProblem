74798bb9e58bdd2613e2eea6feef0f3abaa87148
abc592 <srikanth.chs123456789@gmail.com>
2025-08-17 22:33:25 +0530

    Added new project

--- src/com/nt/cfgs/AppConfig.java
+++ src/com/nt/cfgs/AppConfig.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 @Configuration
 @ComponentScan(basePackages = "com.nt.sbeans")
 public class AppConfig {
@@ -17,6 +18,25 @@ public class AppConfig {
 	public LocalDate createDate() {
 		System.out.println("AppConfig.createDate()");
 		return LocalDate.now();
+		
+	}
+	@Bean(name="ldate1")
+	@Primary
+	public LocalDate createDate1() {
+		System.out.println("AppConfig.createDate()");
+		return LocalDate.of(2020,12,31);
+	}
+		
+	@Bean(name="ldate2")
+	public LocalDate createDate2() {
+		System.out.println("AppConfig.createDate()");
+		return LocalDate.of(2025,12,31);
+		
+	}
+	@Bean(name="ldate3")
+	public LocalDate createDate3() {
+		System.out.println("AppConfig.createDate()");
+		return LocalDate.of(2024,12,31);
 		
 	}
 
--- src/com/nt/sbeans/WeekDayFinder.java
+++ src/com/nt/sbeans/WeekDayFinder.java
@@ -3,18 +3,43 @@ package com.nt.sbeans;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("wx")
 public class WeekDayFinder {
-	
 	@Autowired
+	@Qualifier("ldate")
 	private LocalDate date;
 	public WeekDayFinder() {
 		System.out.println("WeekDayFinder.WeekDayFinder():0-parm");
 	}
+	
+	//for constructor inject
+	@Autowired
+	public WeekDayFinder(@Qualifier("ldate1")LocalDate date) {
+		this.date=date;
+		
+	}
+	
+	//for setter injection
+	@Autowired
+	@Qualifier("ldate2")
+	public void setDate(LocalDate date) {
+		System.out.println("WeekDayFinder.setDate()");
+		this.date=date;
+	}
+	
+	//for arbitrary method injection
+	@Autowired
+	@Qualifier("ldate3")
+	public void putData(LocalDate date) {
+		System.out.println("WeekDayFinder.putData()");
+		this.date=date;
+		
+	}
 	public String showGreetMessage(String name) {
-		System.out.println("WeekDayFinder.showGreetMessage()");
+		System.out.println("WeekDayFinder.showGreetMessage()"+date);
 		int week =date.getDayOfWeek().getValue();
 		if(week!=6 && week!=7)
 			return "Happy Working Day,Working is Worshipping::"+name;
