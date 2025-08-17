611e5831824d28ab2a1a47da3359f4d74a856e49
abc592 <srikanth.chs123456789@gmail.com>
2025-08-17 16:13:27 +0530

    updated the files

--- src/com/nt/sbeans/SeasonFinder.java
+++ src/com/nt/sbeans/SeasonFinder.java
@@ -1,20 +1,60 @@
 package com.nt.sbeans;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("sf")
 public class SeasonFinder {
-    private LocalDate date;
-    @Autowired
+//	@Autowired
+    private LocalDate date; //field injection ---1st way
+//	@Autowired
+    private LocalTime time; //field injection
+	
+	
+	//below code for setterinjection == 2nd way
+    /*@Autowired
     public void setDate(LocalDate date) {
         System.out.println("SeasonFinder.setDate()");
         this.date = date;
     }
     
+    @Autowired
+    public void setTime(LocalTime time) {
+        System.out.println("SeasonFinder.setTime()");
+        this.time = time;
+    }
+    */
+	
+	//below code of arbitary methods for setterinjection --3rd way
+	/*@Autowired
+    public void assignDate(LocalDate date) {
+        System.out.println("SeasonFinder.assignDate()");
+        this.date = date;
+    }
+    
+    @Autowired
+    public void putTime(LocalTime time) {
+        System.out.println("SeasonFinder.putTime()");
+        this.time = time;
+    }
+    
+    */
+    
+	private SeasonFinder() {
+		System.out.println("SeasonFinder.SeasonFinder()::0 param");
+	}
+	//below code of ConstructorInjection ----4th way
+	@Autowired
+	private SeasonFinder(LocalDate date,LocalTime time) {
+		System.out.println("SeasonFinder.SeasonFinder()::2 param");
+		this.date=date;
+		this.time=time;
+	}
     public String showSeasonName() {
+    	System.out.println("SeasonFinder.showSeasonName()"+date+"...."+time);
         int month = date.getMonthValue();
 
         if (month >= 3 && month <= 6) {
